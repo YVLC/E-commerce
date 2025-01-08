3ee25ee050a8ad257f4750839b35129fe6752d7c
Vlad <vladcechina@gmail.com>
2025-01-08 15:44:51 +0200

    Some fixes

---
+++ DataEntities/OrderRecords.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataEntities
+{
+    public record OrderRecords(
+        Guid OrderNumber,
+        DateTime Date,
+        string Status,
+        string City,
+        string Country,
+        string Street,
+        string PostalCode,
+        Guid ClientId,
+        decimal Total,
+        OrderItem[] OrderItems);
+}
